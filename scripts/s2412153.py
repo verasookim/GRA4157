4b37597955ee9e8900795d6c622964799d96b55f
verasookim <97608174+verasookim@users.noreply.github.com>
2024-09-27 13:54:55 +0200

    Update s2412153.py

--- scripts/s2412153.py
+++ scripts/s2412153.py
@@ -1,23 +1,2 @@
-"""Module providing a function printing python version."""
-
-import pandas as pd
-import numpy as np
-
-#2
-exam_data = {'name': ['Anastasia', 
-                      'Dima', 
-                      'Katherine', 
-                      'James', 
-                      'Emily', 
-                      'Michael', 
-                      'Matthew', 
-                      'Laura', 
-                      'Kevin', 
-                      'Jonas'],
-'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
-'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
-'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
-labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-
-df = pd.DataFrame(exam_data , index=labels)
-print(df)
+a = 2
+print(a)
