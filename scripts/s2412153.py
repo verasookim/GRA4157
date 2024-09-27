77889a37a3398e5964d340cd83f30784e17a5542
verasookim <97608174+verasookim@users.noreply.github.com>
2024-09-27 13:41:21 +0200

    Update s2412153.py

--- scripts/s2412153.py
+++ scripts/s2412153.py
@@ -1,19 +1,12 @@
-import sys
+import pandas as pd
+import numpy as np
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 3:
-    print("Usage: python ball_cml.py <v0> <t>")
-    sys.exit(1)
+#2
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-# Read v0 and t from the command line
-v0 = float(sys.argv[1])
-t = float(sys.argv[2])
-
-# Define the gravitational constant
-g = 9.81
-
-# Calculate the formula y(t) = v0*t - 0.5*g*t**2
-y = v0 * t - 0.5 * g * t ** 2
-
-# Print the result
-print(f"The height y after {t} seconds is {y:.2f} meters.")
+df = pd.DataFrame(exam_data , index=labels)
+print(df)
