7c1a51db8777585d251f3ce7bbdf25752eaab9de
verasookim <97608174+verasookim@users.noreply.github.com>
2024-09-27 13:51:42 +0200

    Update s2412153.py

--- scripts/s2412153.py
+++ scripts/s2412153.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 
 #2
-exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 
+                      'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
